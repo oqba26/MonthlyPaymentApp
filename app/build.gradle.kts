5d80bb6f6a19eb7053d78a7ca47bac112b4f2496
oqba26 <oqba26@gmail.com>
2026-08-05 03:54:29 +0330

    بهبود طراحی دیالوگ آپدیت و رفع مشکل برش خوردگی دکمه‌ها

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.oqba26.monthlypaymentapp"
         minSdk = 25
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
     }
